f6ab69159d9a2ec5131579751a3e161d0302d2ac
xantear <tom.tm6@gmail.com>
2019-03-19 19:38:10 -0500

    Main Menu updates
    Made main menu updates with the background and the prelim needed for the audio. Better organization of the folders in the assets.

---
+++ Assets/Scripts/MainMenu/Quit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        Debug.Log("Game is Exiting");
+#else
+        Application.Quit();
+#endif
+    }
+}
