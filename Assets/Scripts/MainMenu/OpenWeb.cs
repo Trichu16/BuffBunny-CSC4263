ab7e27bd66ae289f97dd4c3f08023331320863f7
xantear <tom.tm6@gmail.com>
2019-03-20 15:08:41 -0500

    Updated Main Menu and Connections
    That^

---
+++ Assets/Scripts/MainMenu/OpenWeb.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenWeb : MonoBehaviour
+{
+    public string webpageURL;
+
+    public void OpenWebpage()
+    {
+        Application.OpenURL(webpageURL);
+    }
+}
