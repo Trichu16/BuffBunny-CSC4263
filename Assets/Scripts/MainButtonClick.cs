00b01d624a1c6688da753159e0de89c1b4bbc390
Lucy-Guo <48029132+Lucy-Guo@users.noreply.github.com>
2019-03-20 10:27:13 -0500

    Added UI Health/TrackPad Images/ Training Room Edits

---
+++ Assets/Scripts/MainButtonClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainButtonClick : MonoBehaviour
+{
+    public GameObject textbox;
+
+    public void ClickTheButton()
+    {
+        textbox.SetActive(true);
+
+    }
+}
