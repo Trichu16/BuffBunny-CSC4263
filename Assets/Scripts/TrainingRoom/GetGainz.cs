e16a8f07ad31575178d9b88544e7f72aa3f59c34
Kareem Abdo <KareemAbdo@98-01-a7-aa-16-75.wlan.lsu.edu>
2019-03-20 17:46:19 -0500

    Created Error Box
    Error Box created

--- Assets/Scripts/TrainingRoom/GetGainz.cs
+++ Assets/Scripts/TrainingRoom/GetGainz.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GetGainz : MonoBehaviour
 {
     public GameObject textBox;
+    public GameObject StatusBox;
     public void ClickTheButton()
     {
-        GlobalWorkout.WorkoutCount -= 1;
-        GlobalGainz.GainzCount += 1;
+        if (GlobalWorkout.WorkoutCount == 0)
+        {
+            StatusBox.GetComponent<Text>().text = "You haven't worked out enough!";
+            StatusBox.GetComponent<Animation>().Play("StatusAnim");
+        }
+        else
+        {
+            GlobalWorkout.WorkoutCount -= 1;
+            GlobalGainz.GainzCount += 1;
+        }
     }
 }
