c0c1730441f19b03ca8fb5945921214c7f7d9dd9
Kareem Abdo <KareemAbdo@Kareems-MacBook-Pro.local>
2019-04-03 19:26:21 -0500

    Tweaks regarding Audio In level
    Tweaks regarding audio in level

--- Assets/Scripts/TrainingRoom/Workout.cs
+++ Assets/Scripts/TrainingRoom/Workout.cs
@@ -5,8 +5,21 @@ using UnityEngine;
 public class Workout : MonoBehaviour
 {
     public GameObject textBox;
+    public AudioSource WorkoutSound1;
+    public AudioSource WorkoutSound2;
+    public int GenerateTone;
+
     public void ClickTheButton()
     {
+        GenerateTone++;
+        if(GenerateTone % 2 == 0)
+        {
+            WorkoutSound2.Play();
+        }
+        else
+        {
+            WorkoutSound1.Play();
+        }
         GlobalWorkout.WorkoutCount += 1;
     }
 }
