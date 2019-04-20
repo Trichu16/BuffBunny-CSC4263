4f2ac712bfe63af57934344753a02022c0630753
Trichu16 <asiantri9@gmail.com>
2019-04-20 00:53:04 -0500

    Fight Scene Updated
    Map scene complete

---
+++ Assets/ObjectFollow.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NameFollow : MonoBehaviour
+{
+
+    public Vector3 pos;
+
+    public GameObject robot;
+    public Camera camera;
+    private Vector3 roboPos;
+    private RectTransform rt;
+    private RectTransform canvasRT;
+    private Vector3 roboScreenPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        roboPos = robot.transform.position;
+
+        rt = GetComponent<RectTransform>();
+        canvasRT = GetComponentInParent<Canvas>().GetComponent<RectTransform>();
+        roboScreenPos = camera.WorldToViewportPoint(robot.transform.TransformPoint(roboPos));
+        rt.anchorMax = roboScreenPos;
+        rt.anchorMin = roboScreenPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        roboScreenPos = camera.WorldToViewportPoint(robot.transform.TransformPoint(roboPos));
+        rt.anchorMax = roboScreenPos;
+        rt.anchorMin = roboScreenPos;
+    }
+}
