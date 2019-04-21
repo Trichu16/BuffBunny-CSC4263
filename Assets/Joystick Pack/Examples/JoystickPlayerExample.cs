2081f444db5f7f6082d925f66ddc370d3b621afd
Trichu16 <asiantri9@gmail.com>
2019-04-21 02:33:02 -0500

    fight scene updates
    worked on healthbar and joystick

--- Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
+++ Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
@@ -6,11 +6,11 @@ public class JoystickPlayerExample : MonoBehaviour
 {
     public float speed;
     public VariableJoystick variableJoystick;
-    public Rigidbody rb;
+    public Rigidbody rb2d;
 
     public void FixedUpdate()
     {
         Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
-        rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        rb2d.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
     }
 }
