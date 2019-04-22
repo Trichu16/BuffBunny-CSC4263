597ff47826bfcd1e885e139661ef57f75bcca6af
Kareem Abdo <KareemAbdo@98-01-a7-aa-16-75.wlan.lsu.edu>
2019-04-22 18:44:16 -0500

    Pushing LoadGame working and clicker scene working
    Got the Game loading to work on the main menu, also put in changes to slightly disable the workout button during gainz button pressing

---
+++ Assets/Scripts/TrainingRoom/GameLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLoader : MonoBehaviour
+{
+    public int SavedWorkout;
+    public int SavedGainz;
+    public int SavedAutoWorkout;
+    public int SavedGrowth;
+    public int SavedValue;
+    public int SavedGrowthLevel;
+    public int SavedWorkoutLevel;
+
+    void Start()
+    {
+        if(MainMenuOptions.isLoading == true)
+        {
+            SavedWorkout = PlayerPrefs.GetInt("Saved Workout");
+            GlobalWorkout.WorkoutCount = SavedWorkout;
+            SavedGainz = PlayerPrefs.GetInt("Saved Gainz");
+            GlobalGainz.GainzCount = SavedGainz;
+            SavedAutoWorkout = PlayerPrefs.GetInt("Saved AutoWorkout");
+            GlobalAutoWorkout.WorkoutPerSec = SavedAutoWorkout;
+            SavedGrowth = PlayerPrefs.GetInt("Saved Growth");
+            GlobalGrowth.GrowthPerSec = SavedGrowth;
+            SavedValue = PlayerPrefs.GetInt("Saved Value");
+            SaveGame.saveValue = SavedValue;
+            SavedGrowthLevel = PlayerPrefs.GetInt("Saved Growth Level");
+            GlobalGrowth.numberOfLevel = SavedGrowthLevel;
+            SavedWorkoutLevel = PlayerPrefs.GetInt("Saved Workout Level");
+            GlobalAutoWorkout.NumberOfLevel = SavedWorkoutLevel;
+
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/TrainingRoom/SaveGame.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveGame : MonoBehaviour
+{
+    public int saveGameGainz;
+    public static int saveValue = 10;
+    public GameObject saveButton;
+    public GameObject saveText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        saveText.GetComponent<Text>().text = "Cost: " + saveValue;
+        saveGameGainz = GlobalGainz.GainzCount;
+        if (saveGameGainz >= saveValue)
+        {
+            saveButton.GetComponent<Button>().interactable = true;
+        }
+        else
+        {
+            saveButton.GetComponent<Button>().interactable = false;
+        }
+    }
+    public void SaveTheGame()
+    {
+        GlobalGainz.GainzCount -= saveValue;
+        PlayerPrefs.SetInt("Saved Workout", GlobalWorkout.WorkoutCount);
+        PlayerPrefs.SetInt("Saved Gainz", GlobalGainz.GainzCount);
+        PlayerPrefs.SetInt("Saved AutoWorkout", GlobalAutoWorkout.WorkoutPerSec);
+        PlayerPrefs.SetInt("Saved Growth", GlobalGrowth.GrowthPerSec);
+        PlayerPrefs.SetInt("Saved Growth Level", GlobalGrowth.numberOfLevel);
+        saveValue *= 2;
+        PlayerPrefs.SetInt("Saved Value", saveValue);
+        PlayerPrefs.SetInt("Saved Workout Level",GlobalAutoWorkout.NumberOfLevel);
+
+    }
+}
