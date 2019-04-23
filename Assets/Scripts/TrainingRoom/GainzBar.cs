f3dd8b913979035ce9cad86e8208a0652a874304
Lucy-Guo <48029132+Lucy-Guo@users.noreply.github.com>
2019-04-22 23:12:03 -0500

    Training

---
+++ Assets/Scripts/TrainingRoom/GainzBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GainzBar : MonoBehaviour
+{
+        Image GainzProg;
+        float maxValue;
+        public static float GainzCount;
+
+        void Start()
+        {
+            GainzProg = GetComponent<Image>();
+        }
+        void Update()
+        {
+            maxValue = (float)GlobalAutoWorkout.AutoValue;
+            GainzCount = (float)GlobalGainz.GainzCount;
+            GainzProg.fillAmount = GainzCount / maxValue;
+        }
+
+    }
